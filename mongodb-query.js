ba18785fc7ed173884c1b798093f7a8e010aab24
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-15 13:48:07 +0200

    query data from DB

--- mongodb-query.js
+++ mongodb-query.js
@@ -1,17 +1,9 @@
-const { ObjectID, MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 // const mongodb = require('mongodb')
 // const MongoClient = mongodb.MongoClient
 
 const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = "task-manager"
-
-const id = new ObjectID()
-console.log(id)
-console.log(id.toHexString())
-console.log(id.toHexString().length)
-console.log(id.id)
-console.log(id.id.length)
-console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -19,67 +11,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: "Ann",
-        age: 21
+    console.log("findOne:")
+    db.collection('users').findOne({
+        name: "Andrew",
+        _id: new ObjectId("620b64a9e22636403deae7b4")
     }, (error, result) => {
         if (error) {
-            return console.log("error happened in insert")
+            return console.log("error happened in findOne")
         }
-        console.log(result.acknowledged)
-        console.log(result.insertedId)
+        console.log(result)
+    })
+
+    console.log("find:")
+    db.collection('users').find({
+        name: "Andrew",
+    }).toArray((error, result) => {
+        if (error) {
+            return console.log("error happened in find")
+        }
+        console.log(result)
     })
 })
-
-// MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-//     if (error) {
-//         return console.log("connection error happened")
-//     }
-//     const db = client.db(databaseName)
-
-//     db.collection('users').insertMany([
-//         {
-//             name: "Jen123",
-//             age: 12
-//         },
-//         {
-//             name: "bob123",
-//             age: 23
-//         }
-//     ], (error, result) => {
-//         if (error) {
-//             return console.log("error happened in insert")
-//         }
-//         console.log(result.acknowledged)
-//         console.log(result.insertedIds)
-//     })
-// })
-
-// MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-//     if (error) {
-//         return console.log("connection error happened")
-//     }
-//     const db = client.db(databaseName)
-
-//     db.collection('tasks').insertMany([
-//         {
-//             description: "Task1",
-//             completed: true
-//         },
-//         {
-//             description: "Task2",
-//             completed: false
-//         },
-//         {
-//             description: "Task3",
-//             completed: false
-//         }
-//     ], (error, result) => {
-//         if (error) {
-//             return console.log("error happened in insert")
-//         }
-//         console.log(result.acknowledged)
-//         console.log(result.insertedIds)
-//     })
-// })
