3b7bc5cbf812e38ea7dd31dc3d5c3a1aeafecdc7
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-15 23:16:51 +0200

    added find routes

--- src/index.js
+++ src/index.js
@@ -20,6 +20,26 @@ app.post('/users', (req, res) => {
         })
 })
 
+app.get('/users', function (req, res) {
+    User.find({}).then(function (result) {
+        res.send(result)
+    }).catch(function (error) {
+        res.status(500).send()
+    })
+})
+
+app.get('/user/:id', function (req, res) {
+    const _id = req.params.id
+    User.findById(_id).then(function (user) {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch(function (error) {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', function (req, res) {
     const task = new Task(req.body)
 
@@ -30,6 +50,26 @@ app.post('/tasks', function (req, res) {
         .catch(function (error) {
             res.status(400).send(error)
         })
+})
+
+app.get('/tasks', function (req, res) {
+    Task.find({}).then(function (result) {
+        res.send(result)
+    }).catch(function (error) {
+        res.status(500).send()
+    })
+})
+
+app.get('/task/:id', function (req, res) {
+    const _id = req.params.id
+    Task.findById(_id).then(function (task) {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch(function (error) {
+        res.status(500).send()
+    })
 })
 
 app.listen(port, function () {
