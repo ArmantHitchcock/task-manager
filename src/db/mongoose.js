6d85b8fa3301dcc0f8b569fcd8d889db347d9af4
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-15 19:52:02 +0200

    mongoose validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,24 +1,55 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = "task-manager-api"
 mongoose.connect(connectionURL + "/" + databaseName)
 
 
-//  mongoose model
+//  mongoose model ______________________________________________________________________________________________________
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('do not use password as your password')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('is not valid email address')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 
 const me = new User({
-    name: "bob",
-    age: 56
+    name: "Ann  ",
+    email: 'akjshdkjGHGJFJFGF@HJGJG.com'
 })
 
 me.save()
@@ -29,19 +60,22 @@ me.save()
         console.log(error)
     })
 
-//  mongoose model
+//  mongoose model ______________________________________________________________________________________________________
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task = new Task({
-    description: "to do 1",
-    completed: false
+    description: "to do 2",
+    completed: true
 })
 
 task.save()
