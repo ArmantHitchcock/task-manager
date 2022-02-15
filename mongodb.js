9be46e2f4eea574cf00a7ca87bf63f750547a515
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-15 12:19:31 +0200

    object id exercise

--- mongodb.js
+++ mongodb.js
@@ -1,26 +1,36 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { ObjectID, MongoClient } = require('mongodb')
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
 
 const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = "task-manager"
 
-// MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-//     if (error) {
-//         return console.log("connection error happened")
-//     }
-//     const db = client.db(databaseName)
+const id = new ObjectID()
+console.log(id)
+console.log(id.toHexString())
+console.log(id.toHexString().length)
+console.log(id.id)
+console.log(id.id.length)
+console.log(id.getTimestamp())
 
-//     db.collection('users').insertOne({
-//         name: "Andrew",
-//         age: 27
-//     }, (error, result) => {
-//         if (error) {
-//             return console.log("error happened in insert")
-//         }
-//         console.log(result.acknowledged)
-//         console.log(result.insertedId)
-//     })
-// })
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log("connection error happened")
+    }
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        _id: id,
+        name: "Ann",
+        age: 21
+    }, (error, result) => {
+        if (error) {
+            return console.log("error happened in insert")
+        }
+        console.log(result.acknowledged)
+        console.log(result.insertedId)
+    })
+})
 
 // MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 //     if (error) {
@@ -46,30 +56,30 @@ const databaseName = "task-manager"
 //     })
 // })
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log("connection error happened")
-    }
-    const db = client.db(databaseName)
+// MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+//     if (error) {
+//         return console.log("connection error happened")
+//     }
+//     const db = client.db(databaseName)
 
-    db.collection('tasks').insertMany([
-        {
-            description: "Task1",
-            completed: true
-        },
-        {
-            description: "Task2",
-            completed: false
-        },
-        {
-            description: "Task3",
-            completed: false
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log("error happened in insert")
-        }
-        console.log(result.acknowledged)
-        console.log(result.insertedIds)
-    })
-})
+//     db.collection('tasks').insertMany([
+//         {
+//             description: "Task1",
+//             completed: true
+//         },
+//         {
+//             description: "Task2",
+//             completed: false
+//         },
+//         {
+//             description: "Task3",
+//             completed: false
+//         }
+//     ], (error, result) => {
+//         if (error) {
+//             return console.log("error happened in insert")
+//         }
+//         console.log(result.acknowledged)
+//         console.log(result.insertedIds)
+//     })
+// })
