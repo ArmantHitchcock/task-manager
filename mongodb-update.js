2d81d5b89dfe846106dc6c4b3c99e2ced1b42b75
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-15 14:27:31 +0200

    update record in DB

---
+++ mongodb-update.js
@@ -0,0 +1,39 @@
+const { MongoClient, ObjectId } = require('mongodb')
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+
+const connectionURL = "mongodb://127.0.0.1:27017"
+const databaseName = "task-manager"
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log("connection error happened")
+    }
+    const db = client.db(databaseName)
+
+    console.log("updateOne:")
+    db.collection('users').updateOne({
+        _id: new ObjectId("620b64a9e22636403deae7b4")
+    }, {
+        $set: {
+            name: "John2"
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+
+    console.log("updateMany:")
+    db.collection('users').updateMany({
+        age: 27
+    }, {
+        $set: {
+            age: 72
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+})
