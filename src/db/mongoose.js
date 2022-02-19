1375009e9e5378c87d636902e82d223c89ea158f
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-19 17:49:12 +0200

    fixed connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 
 const connectionURL = process.env.MONGODB_URL
 const databaseName = process.env.DATABASE_NAME
-mongoose.connect(connectionURL + "/" + databaseName)
+mongoose.connect(connectionURL)
