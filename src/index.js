fa547cfd021de57841fcee71df79bfbf0f425904
Armant Hitchcock <armant.hitchcock@gmail.com>
2022-02-19 13:58:29 +0200

    added simple home page

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+//route handlers
+app.get("", function (req, res) {
+    res.send("home page")
+})
+
 app.listen(port, function () {
     console.log("Server is running on port: " + port)
 })
